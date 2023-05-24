4433c32670fe9a195d19cf79c5110134dc883f18
Gyula <Gyula>
2023-05-24 21:20:15 +0200

    Megoldás.

---
+++ Operatorok/Operatorok.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorok
+{
+    internal class Operatorok
+    {
+        int elsoOperandus;
+        string szovegesOperator;
+        int masodikOperandus;
+
+        public Operatorok(int elsoOperandus, string szovegesOperator, int masodikOperandus)
+        {
+            this.elsoOperandus = elsoOperandus;
+            this.szovegesOperator = szovegesOperator;
+            this.masodikOperandus = masodikOperandus;
+        }
+
+        public int ElsoOperandus { get => elsoOperandus; }
+        public string SzovegesOperator { get => szovegesOperator; }
+        public int MasodikOperandus { get => masodikOperandus; }
+
+        public static string Szamol(string kifejezes)
+        {
+            try
+            {
+                string[] kifejezesSplit = kifejezes.Split();
+                switch (kifejezesSplit[1])
+                {
+                    case "mod":
+                        return $"{kifejezes} = {Convert.ToDouble(kifejezesSplit[0]) % Convert.ToDouble(kifejezesSplit[2])}";
+                    case "/":
+                        return $"{kifejezes} = {Convert.ToDouble(kifejezesSplit[0]) / Convert.ToDouble(kifejezesSplit[2])}";
+                    case "div":
+                        return $"{kifejezes} = {int.Parse(kifejezesSplit[0]) / int.Parse(kifejezesSplit[2])}";
+                    case "-":
+                        return $"{kifejezes} = {Convert.ToDouble(kifejezesSplit[0]) - Convert.ToDouble(kifejezesSplit[2])}";
+                    case "*":
+                        return $"{kifejezes} = {Convert.ToDouble(kifejezesSplit[0]) * Convert.ToDouble(kifejezesSplit[2])}";
+                    case "+":
+                        return $"{kifejezes} = {Convert.ToDouble(kifejezesSplit[0]) + Convert.ToDouble(kifejezesSplit[2])}";
+                    default:
+                        return $"{kifejezes} = Hibás operátor!";
+                }
+            }
+            catch (Exception)
+            {
+                return $"{kifejezes} = Egyéb hiba!";
+                throw;
+            }
+
+        }
+    }
+}
--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Operatorok> operatorokLista = new List<Operatorok>();
+
+            StreamReader streamReader = new StreamReader("kifejezesek.txt");
+            while (!streamReader.EndOfStream)
+            {
+                string[] sor = streamReader.ReadLine().Split();
+                operatorokLista.Add(new Operatorok(int.Parse(sor[0]), sor[1], int.Parse(sor[2])));
+            }
+            streamReader.Close();
+
+            Console.WriteLine($"2. feladat: Kifejezések száma: {operatorokLista.Count}");
+            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {operatorokLista.Count(x => x.SzovegesOperator == "mod")}");
+            Console.WriteLine($"4. feladat: {(operatorokLista.Any(x => x.ElsoOperandus % 10 == 0 && x.MasodikOperandus % 10 == 0) ? "Van ilyen kifejezés!" : "Nincs ilyen kifejezés!")}");
+            Console.WriteLine("5. feladat: Statisztika");
+            Console.WriteLine($"\tmod -> {operatorokLista.Count(x => x.SzovegesOperator == "mod")} db");
+            Console.WriteLine($"\t/ -> {operatorokLista.Count(x => x.SzovegesOperator == "/")} db");
+            Console.WriteLine($"\tdiv -> {operatorokLista.Count(x => x.SzovegesOperator == "div")} db");
+            Console.WriteLine($"\t- -> {operatorokLista.Count(x => x.SzovegesOperator == "-")} db");
+            Console.WriteLine($"\t* -> {operatorokLista.Count(x => x.SzovegesOperator == "*")} db");
+            Console.WriteLine($"\t+ -> {operatorokLista.Count(x => x.SzovegesOperator == "+")} db");
+            string bekeres;
+            do
+            {
+                Console.Write("7. feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+                bekeres = Console.ReadLine();
+                if (bekeres != "vége")
+                {
+                    Console.WriteLine($"\t{Operatorok.Szamol(bekeres)}");
+                }
+            } while (bekeres != "vége");
+            Console.WriteLine("8. feladat: eredmenyek.txt");
+            StreamWriter streamWriter = new StreamWriter("eredmenyek.txt");
+            for (int index = 0; index < operatorokLista.Count; index++)
+            {
+                streamWriter.WriteLine($"{Operatorok.Szamol(operatorokLista[index].ElsoOperandus + " " + operatorokLista[index].SzovegesOperator + " " + operatorokLista[index].MasodikOperandus)}");
+            }
+            streamWriter.Close();
         }
     }
 }
